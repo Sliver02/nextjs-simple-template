7593ab9150e18bdc124524e0beb19756b29575db
Jacopo Panzera <jacopo@goviceversa.com>
2026-06-19 21:50:12 +0200

    next config images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    qualities: [75, 85],
+    minimumCacheTTL: 2678400, // 31 days
+  },
 };
 
 export default nextConfig;
