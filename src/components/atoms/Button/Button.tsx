e79701661876cc1d55623bea0783961781ff5182
Jacopo Panzera <jacopo@goviceversa.com>
2026-06-20 14:56:37 +0200

    :button icons

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -6,35 +6,35 @@ import { ReactNode } from "react";
 import styles from "./Button.module.scss";
 
 export interface ButtonProps extends ButtonPrimitive.Props {
-  variant?: "contained" | "outlined" | "text";
-  size?: "small" | "medium" | "large";
-  fullWidth?: boolean;
-  startIcon?: ReactNode;
-  endIcon?: ReactNode;
+	variant?: "contained" | "outlined" | "text";
+	size?: "small" | "medium" | "large";
+	fullWidth?: boolean;
+	icon?: ReactNode;
+	iconPosition?: "start" | "end";
 }
 
 export const Button = ({
-  variant = "contained",
-  size = "medium",
-  fullWidth,
-  startIcon,
-  endIcon,
-  className,
-  children,
-  ...props
+	variant = "contained",
+	size = "medium",
+	fullWidth,
+	icon,
+	iconPosition = "start",
+	className,
+	children,
+	...props
 }: ButtonProps) => (
-  <ButtonPrimitive
-    className={classNames(
-      styles.button,
-      styles[variant],
-      styles[size],
-      { [styles.fullWidth]: fullWidth },
-      className,
-    )}
-    {...props}
-  >
-    {startIcon && <span className={styles.startIcon}>{startIcon}</span>}
-    {children}
-    {endIcon && <span className={styles.endIcon}>{endIcon}</span>}
-  </ButtonPrimitive>
+	<ButtonPrimitive
+		className={classNames(
+			styles.button,
+			styles[variant],
+			styles[size],
+			{ [styles.fullWidth]: fullWidth },
+			className
+		)}
+		{...props}
+	>
+		{icon && iconPosition === "start" && <span className={styles.icon}>{icon}</span>}
+		{children}
+		{icon && iconPosition === "end" && <span className={styles.icon}>{icon}</span>}
+	</ButtonPrimitive>
 );
