3af70c919f478f9e132df7426f27e27b875816fb
Jacopo Panzera <jacopo@goviceversa.com>
2026-06-20 15:03:26 +0200

    removed unused interfaces

--- src/common/globalInterfaces.ts
+++ src/common/globalInterfaces.ts
@@ -1,54 +1,22 @@
 import { ReactNode } from "react";
 
-export enum Color {
-  primary = "primary",
-  neutral = "neutral",
-  accent = "accent",
-  warning = "warning",
-  success = "success",
-  error = "error",
-}
-
-export enum Status {
-  default = "default",
-  success = "success",
-  warning = "warning",
-  error = "error",
-  loading = "loading",
-}
-
-export enum Size {
-  xs = "xs",
-  sm = "sm",
-  md = "md",
-  lg = "lg",
-  xl = "xl",
-  xxl = "xxl",
-}
-
-export enum Surface {
-  raised = "raised",
-  border = "border",
-  flat = "flat",
-}
-
 export interface BaseProps {
-  id?: string;
-  children?: ReactNode;
-  className?: string;
-  style?: React.CSSProperties;
+	id?: string;
+	children?: ReactNode;
+	className?: string;
+	style?: React.CSSProperties;
 }
 
 export interface LinkProps {
-  id?: string;
-  href: string;
-  title?: string;
-  target?: string;
-  label?: string;
-  download?: boolean | string;
+	id?: string;
+	href: string;
+	title?: string;
+	target?: string;
+	label?: string;
+	download?: boolean | string;
 }
 
 export interface AlertResponse {
-  severity: "success" | "warning" | "error";
-  text: string;
+	severity: "success" | "warning" | "error";
+	text: string;
 }
